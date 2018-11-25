b58a24020f8fbb886fe45758b46aefbba01c5c33
Derek Lao <dlao00@stuy.edu>
2018-11-25 13:22:49 -0500

    put in correct method shell for compareTo, and added correct comments from documentation

--- MyString.java
+++ MyString.java
@@ -3,4 +3,29 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
   public MyString(CharSequence s){
 
   }
+
+  //Returns the char value at the specified index.
+  public char charAt(int index) throws IndexOutOfBoundsException{
+
+  }
+
+  //Returns the length of this character sequence.
+  public int length(){
+
+  }
+
+  //Returns a CharSequence that is a subsequence of this sequence.
+  public CharSequence subSequence(int start, int end) throws IndexOutOfBoundsException{
+
+  }
+
+  //Returns a string containing the characters in this sequence in the same order as this sequence.
+  public String toString(){
+
+  }
+
+  //Compares this object with the specified object for order.
+  public int compareTo(CharSequence o){
+
+  }
 }
